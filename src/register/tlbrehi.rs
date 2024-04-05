bbba29bc7c20d38938b33c21f49bec9d5f0ea2bd
Helloworld-lbl <2304528821@qq.com>
2024-04-05 18:23:39 +0800

    Add `tlbrehi.rs`.

---
+++ src/register/tlbrehi.rs
@@ -0,0 +1,45 @@
+use bit_field::BitField;
+
+/// Tlbrehi register
+#[derive(Clone, Copy)]
+pub struct Tlbrehi {
+    bits: usize,
+}
+
+impl Tlbrentry {
+    #[inline]
+    pub fn bits(&self) -> usize {
+        self.bits
+    }
+
+    #[inline]
+    pub fn ps(&self) -> usize {
+        self.bits.get_bits(0..6)
+    }
+
+    #[inline]
+    pub fn set_ps(&mut self, value: usize) {
+        self.bits.set_bits(0..6, value);
+    }
+
+    #[inline]
+    pub fn write_ps(&mut self, pa: usize) {
+        self.set_ps(pa);
+        self.write();
+    }
+
+    #[inline]
+    pub fn write(&self) {
+        unsafe {
+            _write(self.bits)
+        }
+    }
+}
+
+read_csr_as!(Tlbrehi, 0x8e, __read_tlbrehi);
+write_csr!(0x8e, __write_tlbrehi);
+
+pub fn write_ps_to_tlbrehi(pa: usize) {
+    let mut tlbrehi = read();
+    tlbrehi.write_pa(pa);
+}
